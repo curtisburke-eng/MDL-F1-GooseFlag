761500c15842cedbdcd857357111826a7471d343
curtisburke-eng <curtisburke.eng@gmail.com>
2024-02-16 14:21:15 -0700

    Add ability to rotate multiple times

--- MDL-F1/src/main.cpp
+++ MDL-F1/src/main.cpp
@@ -1,25 +1,29 @@
 #include <Arduino.h>
 #include <wiringPi.h>
 
-#define STEPPER_PIN_1 1                                       // GPIO pin connected to stepper motor driver (step pin)
-#define STEPPER_PIN_2 2                                       // GPIO pin connected to stepper motor driver (direction pin)
+#define MOTOR_STEP_PIN 1                                       // GPIO pin connected to stepper motor driver (step pin)
+#define MOTOR_DIR_PIN 2                                       // GPIO pin connected to stepper motor driver (direction pin)
 #define STEPS_PER_REVOLUTION 200                              // Number of steps required for 1 full revolution of the stepper motor
 
 void setup() {
     wiringPiSetup();
-    pinMode(STEPPER_PIN_1, OUTPUT);
-    pinMode(STEPPER_PIN_2, OUTPUT);
+    pinMode(MOTOR_STEP_PIN, OUTPUT);
+    pinMode(MOTOR_DIR_PIN, OUTPUT);
 
 }
 
-void rotateMotorFullRev() {
-    // Rotate stepper motor for 1 full revolution
-    digitalWrite(STEPPER_PIN_2, HIGH);                        // Set direction
-    for (int i = 0; i < STEPS_PER_REVOLUTION; i++) {
-        digitalWrite(STEPPER_PIN_1, HIGH);
-        delayMicroseconds(500);                               // Adjust delay for your motor
-        digitalWrite(STEPPER_PIN_1, LOW);
-        delayMicroseconds(500);                               // Adjust delay for your motor
+void rotateMotorFullRev(int numRotations) {
+    digitalWrite(MOTOR_DIR_PIN, HIGH);                        // Set direction
+
+    // Rotate 1 full rotation X times
+    for (int i = 0; i < numRotations; i++) {
+      // Rotate motor 1 full revolution
+      for (int j = 0; j < STEPS_PER_REVOLUTION; j++) {
+          digitalWrite(MOTOR_STEP_PIN, HIGH);
+          delayMicroseconds(500);                               // Adjust delay for your motor
+          digitalWrite(MOTOR_STEP_PIN, LOW);
+          delayMicroseconds(500);                               // Adjust delay for your motor
+      }
     }
 }
 
@@ -30,7 +34,7 @@ int main() {
     // Start infinite loop
     while (true) {
         // Rotate motor X full rotations
-        rotateMotorFullRev();
+        rotateMotorFullRev(3);
         delay(100);                                           // Adjust delay for responsiveness
     }
 
