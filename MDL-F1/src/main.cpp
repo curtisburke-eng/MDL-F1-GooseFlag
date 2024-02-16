bc7d363ea584dd6915778852b07e966e75c49072
curtisburke-eng <curtisburke.eng@gmail.com>
2024-02-16 14:17:16 -0700

    Create rotateMotor function

--- MDL-F1/src/main.cpp
+++ MDL-F1/src/main.cpp
@@ -1,18 +1,38 @@
 #include <Arduino.h>
+#include <wiringPi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define STEPPER_PIN_1 1                                       // GPIO pin connected to stepper motor driver (step pin)
+#define STEPPER_PIN_2 2                                       // GPIO pin connected to stepper motor driver (direction pin)
+#define STEPS_PER_REVOLUTION 200                              // Number of steps required for 1 full revolution of the stepper motor
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    wiringPiSetup();
+    pinMode(STEPPER_PIN_1, OUTPUT);
+    pinMode(STEPPER_PIN_2, OUTPUT);
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void rotateMotorFullRev() {
+    // Rotate stepper motor for 1 full revolution
+    digitalWrite(STEPPER_PIN_2, HIGH);                        // Set direction
+    for (int i = 0; i < STEPS_PER_REVOLUTION; i++) {
+        digitalWrite(STEPPER_PIN_1, HIGH);
+        delayMicroseconds(500);                               // Adjust delay for your motor
+        digitalWrite(STEPPER_PIN_1, LOW);
+        delayMicroseconds(500);                               // Adjust delay for your motor
+    }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+int main() {
+    // Run the setup procedure
+    setup();
+
+    // Start infinite loop
+    while (true) {
+        // Rotate motor X full rotations
+        rotateMotorFullRev();
+        delay(100);                                           // Adjust delay for responsiveness
+    }
+
+    return 0;
 }
