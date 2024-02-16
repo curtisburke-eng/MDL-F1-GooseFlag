9236ccfe01d38cb37b9208db0fb5686ced1f00b1
curtisburke-eng <curtisburke.eng@gmail.com>
2024-02-16 14:26:49 -0700

    Add RF receiver logic

--- MDL-F1/src/main.cpp
+++ MDL-F1/src/main.cpp
@@ -1,15 +1,21 @@
 #include <Arduino.h>
 #include <wiringPi.h>
+#include <rc-switch/RCSwitch.h>
 
 #define MOTOR_STEP_PIN 1                                       // GPIO pin connected to stepper motor driver (step pin)
 #define MOTOR_DIR_PIN 2                                       // GPIO pin connected to stepper motor driver (direction pin)
 #define STEPS_PER_REVOLUTION 200                              // Number of steps required for 1 full revolution of the stepper motor
+#define RF_RECEIVER_PIN 0                                     // GPIO pin connected to the RF receiver
+
 
 void setup() {
     wiringPiSetup();
     pinMode(MOTOR_STEP_PIN, OUTPUT);
     pinMode(MOTOR_DIR_PIN, OUTPUT);
 
+    // Initialize RF receiver
+    RCSwitch rfReceiver = RCSwitch();
+    rfReceiver.enableReceive(RF_RECEIVER_PIN);
 }
 
 void rotateMotorFullRev(int numRotations) {
@@ -33,9 +39,12 @@ int main() {
 
     // Start infinite loop
     while (true) {
+      // New RF signal is detected
+      if (digitalRead(RF_RECEIVER_PIN) == HIGH) {
         // Rotate motor X full rotations
         rotateMotorFullRev(3);
         delay(100);                                           // Adjust delay for responsiveness
+      }
     }
 
     return 0;
