97fefe018e5277e46d86c59a8969912c15ccea4a
curtisburke-eng <curtisburke.eng@gmail.com>
2024-02-29 23:42:38 -0700

    Move functions within the class

--- MDL-F1/include/F1_typ.h
+++ MDL-F1/include/F1_typ.h
@@ -13,13 +13,17 @@ public:
     F1_command_typ cmd;
     F1_status_typ status;
     F1_internal_typ internal;
+
+    void Init();
+    void loadDefaultConfig();
+    void loadCustomConfig();
 };
 
 struct F1_command_typ
 {
     // Commands
     bool useSerialComms;                                        // Command for determining if Serial output is to be used
-    bool useConfig;                                             // Command for determining the source of initialvalues for all configurable variables
+    bool useCustomConfig;                                       // Command for determining the source of initial values for all configurable variables
 };
 
 struct F1_status_typ
@@ -31,22 +35,19 @@ struct F1_status_typ
 struct F1_internal_typ
 {   
     // Configuration variables
-    std::string configFileName = "configuration.csv";
+    String configFileName = "config.json";
     
     // Pin Type variables
-    F1_pin_typ motorEnable;                                     // GPIO pin connected to stepper motor driver (step/enable/power pin)
-    F1_pin_typ motorDirection;                                  // GPIO pin connected to stepper motor driver (direction pin)
-    F1_pin_typ rfReceiver;                                      // GPIO pin connected to the RF receiver or DI from RF receiver 
+    int motorDriverPinIN1;                                      // GPIO pin connected to stepper motor driver IN1 pin
+    int motorDriverPinIN2;                                      // GPIO pin connected to stepper motor driver IN2 pin
+    int motorDriverPinIN3;                                      // GPIO pin connected to stepper motor driver IN3 pin
+    int motorDriverPinIN4;                                      // GPIO pin connected to stepper motor driver IN4 pin
+    int rfReceiverPin;                                          // GPIO pin connected to RF controller output
 
     // Motor Configuration variables
     int stepsPerRev;                                            // The number of steps for a given motor to complete one full revolution
     int revsPerCycle;                                           // The number of revolutions desired for one cycle (1 cycle being started by an RF input)
-};
-
-struct F1_pin_typ
-{
-    int value;
-    int pin;
+    int rpm;                                                    // The speed of rotation in revolutions per min.
 };
 
 
